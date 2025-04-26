b677c34f4d10280dcc334f9635fb744f29801cbd
Fadlin Azhary <fadlinsiregar@gmail.com>
2025-04-26 10:17:31 +0700

    Add filter for disable Gutenberg for post type page

--- app/filters.php
+++ app/filters.php
@@ -50,3 +50,10 @@ add_filter('comment_form_defaults', function ($defaults) {
 
     return $defaults;
 });
+
+add_filter('use_block_editor_for_post_type', function($use_block_editor, $post_type) {
+    if ($post_type === 'page') {
+        return false;
+    }
+    return $use_block_editor;
+}, 10, 2);
